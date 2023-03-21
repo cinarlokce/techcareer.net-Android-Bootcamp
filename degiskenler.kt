0a99c6b0381f3694b61648c69a4e0c1b08517fba
cinarlokce <92788196+cinarlokce@users.noreply.github.com>
2023-03-21 12:37:13 +0300

    Android Bootcamp kapsamındaki ilk ödev

---
+++ degiskenler.kt
@@ -0,0 +1,48 @@
+package com.cinarlokce.kotlindersleriodev1
+
+fun main(){
+    val sehir="Adana"
+    val ulke="Türkiye"
+    val telefon=5322221111
+    val postaKodu=1170
+    val email="email@example.com"
+    val meslek="Engineer"
+    var stokMiktari=36
+    val musteriAdi="Ahmet"
+    var bakiye=65234
+    val dogumGunu="01.01.2022"
+    var maas=8500
+    var medeniDurum="Bekar"
+    val odeme="Kredi Kartı"
+    var siparisAdeti=67
+    var arabaModeli="Astra"
+    val kitapAdi="Martı"
+    val yayinlamaTarihi="01.01.2018"
+    val indirimMiktari=35/100
+    val odaSayisi=4
+    val enlem="36-42"
+    val boylam="26-45"
+    val urunAdi="Araba"
+    var yemekFiyati=96
+    val marka="Opel"
+    val muzikAdi="MothToAFlame"
+    val videoSuresi="2:48"
+    val urunPuani=5
+    val resimAdi="Kaplumbağa Terbiyecisi"
+    val dosyaFormati=".dll"
+    val renk="Mavi"
+    val renkKodu=255
+    var telefonModeli="S23 Ultra"
+    var ekranBoyutu="6.8 inches"
+    var agirlik="235 gram"
+    val ulusalGun="23 Nisan"
+    val tatilGunu="Cumartesi"
+    val rezervasyonTarihi="12.06.2023"
+    val sokakAdi="Menekşe Sokak"
+    val otobusHatti="144"
+    var kalanDakika=45
+    val takipKodu="34215678"
+    var kuponSuresi=92
+    val kuponKodu=431689064
+    val faturaAdresi="İstanbul"
+}
